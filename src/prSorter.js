040d010815defce274b014c27469b7307eaf9e0a
oryan <oryan.mishali@gmail.com>
2016-09-12 00:24:57 +0300

    ability to change num of additional pages and sort

--- src/prSorter.js
+++ src/prSorter.js
@@ -1,23 +1,28 @@
 function PRSorter (window, $) {
 	var lis = [];
-	var sortDesc = true;
+	var originalLis = [];
+	var sortAsc = true;
+	var numOfPagesDirty = undefined;
 	var numOfMorePagesInput 	= '<div class="float-left select-menu js-menu-container js-select-menu">' +
 									'<input class="form-control subnav-search-input" type="number" min="0" id="morePages" placeholder="# of additional pages" style="width: 145px; margin-top: 7px; padding: 0 5px;" />' +
 								  '</div>';
 	var sortButton 				= '<div id="sortByMerged" class="float-left select-menu js-menu-container js-select-menu">' +
-									'<button class="btn-link js-menu-target" style="color: #090; margin-left: 5px;">Sort by Merged</button>' +
+									'<button class="btn-link" style="color: #090; margin-left: 5px;">Sort by Merged</button>' +
 								  '</div>';
 
 	function bindEvents () {
 		if (window.location.pathname.indexOf('/pulls') > -1) {
 			lis = [];
-			sortDesc = true;
+			sortAsc = true;
 
 			var buttonsBox = $('.table-list-header .table-list-header-toggle:eq(1)');
 			buttonsBox.prepend(sortButton);
 			buttonsBox.prepend(numOfMorePagesInput);
 
 			$('#sortByMerged').on('click', onSortByMerged);
+
+			var ul = $('ul.Box-body');
+			originalLis = ul.children('li').clone();
 		}
 	}
 
@@ -26,22 +31,28 @@ function PRSorter (window, $) {
 	}
 
 	function onSortByMerged () {
-		sortDesc = !sortDesc;
+		$('ul.Box-body').empty();
+		var pagesToLoad = parseInt($('#morePages').val()) || 0;
 
-		if (lis.length === 0) {
-			var ul = $('ul.Box-body');
-			lis = ul.children('li');
+		if (pagesToLoad !== numOfPagesDirty) {
 
-			var numOfMorePages = parseInt($('#morePages').val()) || 0;
-			if (numOfMorePages > 0) {
-				loadMorePages(numOfMorePages)
-					.then(function () {
-						sortLis();
-					});
+			numOfPagesDirty = pagesToLoad;
+			lis = originalLis.clone();
+			sortAsc = false;
 
-			} else {
-				sortLis();
-			}
+		} else {
+
+			sortAsc = !sortAsc;
+			pagesToLoad = 0;
+		}
+
+		if (pagesToLoad > 0) {
+
+			loadMorePages(pagesToLoad)
+				.then(function () {
+					sortLis();
+				});
+
 		} else {
 			sortLis();
 		}
@@ -52,7 +63,7 @@ function PRSorter (window, $) {
 			var aDatetime = $(a).find('relative-time').attr('datetime');
 			var bDatetime = $(b).find('relative-time').attr('datetime');
 
-			if (sortDesc) {
+			if (sortAsc) {
 				return moment(aDatetime, "YYYY-MM-DDTHH:mm:ss").valueOf() - moment(bDatetime, "YYYY-MM-DDTHH:mm:ss").valueOf();
 			} else {
 				return moment(bDatetime, "YYYY-MM-DDTHH:mm:ss").valueOf() - moment(aDatetime, "YYYY-MM-DDTHH:mm:ss").valueOf();
